ffafb8a5d0dd0bb0a68dd534453d54ea2fe7624c
Filippo Graziano <filippograziano13@gmail.com>
2026-07-30 17:39:26 +0200

    add list of products

--- pages/jsx-pages/Products.jsx
+++ pages/jsx-pages/Products.jsx
@@ -1,10 +1,39 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios'
 import '../css-pages/Products.css'
 
-const Products = () => (
+const Products = () => {
 
-    <>
-        <h2>Prodotti</h2>
-    </>
-);
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+
+        axios.get(`https://fakestoreapi.com/products`)
+            .then(res => setProducts(res.data))
+
+    }, [])
+
+    return (
+        <>
+
+            <h2>Prodotti</h2>
+
+            <ul>
+                {products.map(el => (
+
+                    <li key={el.id}>
+
+                        <h4>{el.title}</h4>
+                        <p className='description'>{el.description}</p>
+                        <span className="price">{el.price}€</span>
+
+                    </li>
+
+                ))}
+            </ul>
+
+        </>
+    );
+};
 
 export default Products
