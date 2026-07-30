f079a1a051d1d534f46b955d670d4af5dda9d97d
Filippo Graziano <filippograziano13@gmail.com>
2026-07-30 17:27:53 +0200

    create a Shell layout

--- pages/jsx-pages/ShellLayout.jsx
+++ pages/jsx-pages/ShellLayout.jsx
@@ -3,19 +3,33 @@ import { Outlet, NavLink } from 'react-router';
 
 const ShellLayout = () => (
 
-    <>
+    <div id='ShallLayout'>
+
         <header>
+
             <nav>
+
                 <NavLink to="/" >Home</NavLink>
                 <NavLink to="AboutUs" >Chi Siamo</NavLink>
                 <NavLink to="Products" >Prodotti</NavLink>
+                
             </nav>
+
         </header>
 
-        <Outlet />
+        <main>
 
-        <footer></footer>
-    </>
+            <Outlet />
+
+        </main>
+
+        <footer>
+
+            <h2>Ciao {`:)`}</h2>
+
+        </footer>
+
+    </div>
 );
 
 export default ShellLayout
