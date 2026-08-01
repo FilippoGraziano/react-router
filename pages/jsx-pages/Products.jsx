f2b9b3d4595d4ed2af24409811e4a7eebdd46df7
Filippo Graziano <filippograziano13@gmail.com>
2026-08-01 15:00:11 +0200

    add a satte for API

--- pages/jsx-pages/Products.jsx
+++ pages/jsx-pages/Products.jsx
@@ -5,33 +5,60 @@ import { Link, useParams } from 'react-router';
 
 const Products = () => {
 
-    const [products, setProducts] = useState([])
+    const [products, setProducts] = useState({ state: `idle` })
 
     useEffect(() => {
 
-        axios.get(`https://fakestoreapi.com/products`)
-            .then(res => setProducts(res.data))
+        setProducts({ ...products, state: `loading` }),
+
+            axios.get(`https://fakestoreapi.com/products`)
+                .then(res => (setProducts({ ...products, state: `success`, result: res.data })))
+                .catch(err => setProducts({ ...products, state: `error` }))
 
     }, [])
+
+    let content
+
+    switch (products.state) {
+
+        case `idle`: content = <></>;
+            break;
+
+        case `loading`: content = <>loading...</>;
+            break;
+
+        case `success`: content = (
+        
+        <ul>
+
+            {products.result.map(el => (
+
+                <li key={el.id}>
+
+                    <h4>{el.title}</h4>
+                    <span className="price">{el.price} €</span>
+                    <Link to={String(el.id)}>Vai alla pagina del prodotto</Link>
+
+                </li>
+
+            ))}
+
+        </ul>
+
+        );
+            break;
+
+        case `error`: content = <>C'è stato un errore</>;
+            break;
+
+    }
 
     return (
         <>
 
             <h2>Prodotti</h2>
 
-            <ul>
-                {products.map(el => (
-
-                    <li key={el.id}>
-
-                        <h4>{el.title}</h4>
-                        <span className="price">{el.price} €</span>
-                        <Link to={String(el.id)}>Vai alla pagina del prodotto</Link>
-
-                    </li>
-
-                ))}
-            </ul>
+            {content}
 
         </>
     );
--- pages/jsx-pages/SingleProduct.jsx
+++ pages/jsx-pages/SingleProduct.jsx
@@ -5,27 +5,58 @@ import { useParams } from 'react-router';
 
 const SingleProduct = () => {
 
-    const [product, setProduct] = useState({})
+    const [product, setProduct] = useState({ state: `idle` })
 
     const { id } = useParams()
 
     useEffect(() => {
+
+        setProduct({ ...product, state: `loading` })
+
         axios.get(`https://fakestoreapi.com/products/${id}`)
-            .then(res => setProduct(res.data))
+            .then(res => setProduct({ ...product, state: `success`, result: res.data }))
+            .catch(err => setProduct({ ...product, state: `error` }))
+
     }, [id])
+
+    let content
+
+    switch (product.state) {
+
+        case `idle`: content = <></>;
+            break;
+
+        case `loading`: content = <>loading...</>;
+            break;
+
+        case `success`: content = (
+
+            <>
+                <h2>{product.result.title}</h2>
+                <span className="category">{product.result.category}</span>
+
+                <img src={product.result.image} alt="product-image" />
+
+                <p className="description">{product.result.description}</p>
+
+                <span className="price">{product.result.price} €</span>
+            </>
+
+        );
+            break;
+
+        case `error`: content = <>C'è stato un errore</>;
+            break;
+
+    }
+
 
     return (
 
         <div id='silgle-product'>
 
-            <h2>{product.title}</h2>
-            <span className="category">{product.category}</span>
+            {content}
 
-            <img src={product.image} alt="product-image" />
-
-            <p className="description">{product.description}</p>
-
-            <span className="price">{product.price} €</span>
         </div>
 
     );
